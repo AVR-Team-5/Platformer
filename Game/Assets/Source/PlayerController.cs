181cd08048e87e10e350c081dfa231a945d9baeb
PunkyIANG <tsoorckanukris@gmail.com>
2021-10-05 12:28:00 +0300

    implemented running + added rider gitignore

--- Game/Assets/Source/PlayerController.cs
+++ Game/Assets/Source/PlayerController.cs
@@ -1,9 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 // TODO: make sure floor trigger gets out of ground at the first frame of the jump
-// otherwise it might get fucky with jump cancels
+// otherwise it might get fucky 
+// as the state machine will end up in an infinite cycle
 
 enum PlayerState
 {
@@ -16,16 +18,23 @@ enum PlayerState
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
 {
-    public float horizontalSpeed;
     public float startVerticalSpeedUp;
     public float jumpGravity;
     public float fallGravity;
+    
+    [Space(10)]
+    public float maxRunningSpeed;
+    public float runAcceleration;
+    public float brakeAcceleration;
+    public float stopAcceleration;
+    public float valueCloseToZero;
 
     GroundController gc;
     Rigidbody2D rb;
     Vector2 horizontalDirection;
-    bool isJumping = false;
+    bool isJumping;
     float currentVerticalSpeed;
+    float currentHorizontalSpeed;
     PlayerState state = PlayerState.Falling;
 
     void Start()
@@ -43,15 +52,13 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector2 finalFrameMovement = Vector2.zero;
-
-        // print("Starting switch");
-
+        // jumping stuff
         switch (state)
         {
             case PlayerState.Grounded:
                 // no gravity calc
                 // exit condition
+                currentVerticalSpeed = 0f;
                 if (isJumping) {
                     state = PlayerState.StartedJump;
                     goto case PlayerState.StartedJump;
@@ -89,31 +96,36 @@ public class PlayerController : MonoBehaviour
                 
                 break;
         }
+        
+        
+        // running stuff
+        // TODO: replace all of these fucking Mathf function calls with something better
+        var runDirection = Input.GetAxisRaw("Horizontal"); // -1, 0, 1, why not give out an int then :<
+        
+        if (Mathf.Abs(runDirection) > valueCloseToZero)   // if there is an input to run
+        {
+            if (Mathf.Abs(currentHorizontalSpeed) < valueCloseToZero // if the player is stationary (horizontally)
+                || runDirection * currentHorizontalSpeed > 0) // or if he continues to run in the same direction as before
+            {
+                currentHorizontalSpeed += runAcceleration * runDirection * Time.deltaTime; //accelerate further
+            }
+            else //otherwise the player is braking
+            {
+                currentHorizontalSpeed += brakeAcceleration * runDirection * Time.deltaTime;
+            }
+        }
+        else    // if no input
+        {
+            // subtract just enough speed so it stops at 0
+            currentHorizontalSpeed -= Mathf.Min(Mathf.Abs(currentHorizontalSpeed), 
+                Mathf.Abs(stopAcceleration * Time.deltaTime)) * Mathf.Sign(currentHorizontalSpeed);
+        }
 
-        // print("Left switch");
+        // limiting the speed to its scripted max
+        currentHorizontalSpeed = Mathf.Clamp(currentHorizontalSpeed, -maxRunningSpeed, maxRunningSpeed);
+        
 
-
-        rb.MovePosition(transform.position + Vector3.up * currentVerticalSpeed * Time.fixedDeltaTime);
-        print(state);
-
-
-        // var finalMovement = horizontalDirection.normalized * horizontalSpeed;
-
-        // if (gc.IsGrounded) {
-
-        // } else {
-
-        // }
-
-        // if (isJumping) {
-        //     if (wasJumpingLastFrame == false)
-        //         currentVerticalSpeed = startVerticalSpeedUp;
-
-        //     currentVerticalSpeed -= jumpGravity * Time.fixedDeltaTime;
-        //     wasJumpingLastFrame = true;
-        // } else {
-        //     currentVerticalSpeed -= fallGravity * Time.fixedDeltaTime;
-        //     wasJumpingLastFrame = false;
-        // }
+        rb.MovePosition(transform.position + currentVerticalSpeed * Time.fixedDeltaTime * Vector3.up
+                                                 + currentHorizontalSpeed * Time.fixedDeltaTime * Vector3.right);
     }
 }
